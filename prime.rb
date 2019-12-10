81a4b0d1cff097e13f90ded41370747e56d8b1bf
Brian Nicholls <nichol88@gmail.com>
2019-12-10 05:39:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(num)
   num = num * -1 if num * -1 == -num
 
-  console.log(num)
+  puts num
   a = [1,2,3,5]
   if !a.include?(num) && num.to_s.end_with?("5") || num % 2 == 0
     return false
