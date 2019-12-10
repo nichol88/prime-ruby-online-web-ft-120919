f914041957fb02a244dff80577db3c542eebd4be
Brian Nicholls <nichol88@gmail.com>
2019-12-10 05:42:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,12 +3,14 @@ def prime?(num)
   num = num * -1 if num * 1 < 0
 
   puts num
-  a = [1,2,3,5]
-  if !a.include?(num) && num.to_s.end_with?("5") || num % 2 == 0
-    return false
-  elsif a.include?(num)
-    return true
-  else
-  end
 
+  a = [1,2,3,5]
+
+  case num
+  when a.include?(num)
+    return true
+  when num.to_s.end_with?("5") || num % 2 == 0
+    return false
+  end
+  
 end
