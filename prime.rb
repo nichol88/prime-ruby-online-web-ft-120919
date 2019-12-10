259b421d0f5b01cdf053b0799781a9cc1a5f6dbb
Brian Nicholls <nichol88@gmail.com>
2019-12-10 05:51:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 # Add  code here!
 def prime?(num)
   a = [1,2,3,5,11,13]
-
-  if a.include?(num)
+  s = num.to_s
+  if a.include?(num) || s.end_with?("7")
     return true
-  elsif num.to_s.end_with?("5") || num % 2 == 0 || num < 0
+  elsif s.end_with?("5") || num % 2 == 0 || num < 0
     return false
   else
     false
