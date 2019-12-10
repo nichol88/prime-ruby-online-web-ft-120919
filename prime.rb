a73b57bdeb4f06768c0c259cd5650124dd9df9cb
Brian Nicholls <nichol88@gmail.com>
2019-12-10 05:53:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(num)
+  a = [2,3,5,11,13]
+  s = num.to_s
+  if a.include?(num) || s.end_with?("7")
+    return true
+  elsif s.end_with?("5") || num % 2 == 0 || num < 0
+    return false
+  else
+    false
+  end
+
+end
