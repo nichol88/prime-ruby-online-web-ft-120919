73abeba22677298ea45940928f667934e0031f97
Brian Nicholls <nichol88@gmail.com>
2019-12-10 05:36:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(num)
   num = num * -1 if num * -1 != -num
   a = [1,2,3,5]
-  if num > 5 && num.to_s.end_with?("5") || num % 2 == 0
+  if !a.include?(num) && num.to_s.end_with?("5") || num % 2 == 0
     return false
   else
     return true
