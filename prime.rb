561928b46271324a0cb08eb7405e4ce38a8d8b56
Brian Nicholls <nichol88@gmail.com>
2019-12-10 05:36:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,11 @@
 # Add  code here!
 def prime?(num)
   num = num * -1 if num * -1 != -num
-  r = [1..num].to_a.select{|e| e % 2 == 1}
-
-  r.each{ |i|
-    if (num / i).to_s.length > 2
-      return false
-    else
-      puts i
-      true
-    end
-  }
+  a = [1,2,3,5]
+  if num > 5 && num.to_s.end_with?("5") || num % 2 == 0
+    return false
+  else
+    return true
+  end
 
 end
