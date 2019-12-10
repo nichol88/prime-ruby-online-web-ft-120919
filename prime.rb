4c819ed96166d8f7880dae0926a1094ae61177dc
Brian Nicholls <nichol88@gmail.com>
2019-12-10 05:30:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 def prime?(num)
   num = num * -1 if num * -1 != -num
-  r = [1..num].to_a.select{|e| e % 2 != 0}
+  r = [1..num].to_a.select{|e| e % 2 == 1}
 
   r.each{ |i|
     if (num / i).to_s.length > 2
